3699e989084c4a5f40ef6cd7fbf3b4c85312ebd8
Ahmed Shafique <99950606+AhmedShafique313@users.noreply.github.com>
2025-01-18 18:01:34 +0500

    project 1 completed

--- supabase based metro ticket project/app.py
+++ supabase based metro ticket project/app.py
@@ -1,9 +1,13 @@
 import os
 from dotenv import load_dotenv
 from supabase import create_client
-from project import stations, ticket_counter
+from project import stations, name_passenger, selected_station, station_price
 load_dotenv()
 
 url = os.environ.get("SUPABASE_URL")
 key = os.environ.get("SUPABASE_KEY")
 supabase = create_client(url, key)
+
+inserting = (supabase.table("metro database").insert({"id": 2, "name": name_passenger, "station name": selected_station, "price": station_price}).execute())
+response = supabase.table('metro database').select("*").execute()
+print(response)
--- supabase based metro ticket project/project.py
+++ supabase based metro ticket project/project.py
@@ -1,24 +1,21 @@
 stations = {'UET':30, 'Sultan Pura':40, 'Railway Station':50}
 
-def ticket_counter():
-    print('Welcome to the Orange line metro station!')
-    name_passenger = input('Please enter your name: ')
-    print(f'Hi, {name_passenger}, Select stations from the list below:')
-    for index, (key, value) in enumerate(stations.items(), start=1):
-        print(f'{index}. {key} Rs.{value}')
-    try:
-        choice = int(input('Enter the number of the station you want to select: '))
-        match choice:
-            case 1:
-                station_name = 'UET'
-            case 2:
-                station_name = 'Sultan Pura'
-            case 3:
-                station_name = 'Railwat Station'
-        selected_station = station_name
-        station_price = stations[station_name]
-        print(f"You selected: {selected_station} with a ticket price of Rs. {station_price}.")
-    except ValueError:
-        print("Invalid input. Please enter a number corresponding to the station.")        
-
-ticket_counter()
+print('Welcome to the Orange line metro station!')
+name_passenger = input('Please enter your name: ')
+print(f'Hi, {name_passenger}, Select stations from the list below:')
+for index, (key, value) in enumerate(stations.items(), start=1):
+    print(f'{index}. {key} Rs.{value}')
+try:
+    choice = int(input('Enter the number of the station you want to select: '))
+    match choice:
+        case 1:
+            station_name = 'UET'
+        case 2:
+            station_name = 'Sultan Pura'
+        case 3:
+            station_name = 'Railwat Station'
+    selected_station = station_name
+    station_price = stations[station_name]
+    print(f"You selected: {selected_station} with a ticket price of Rs. {station_price}.")
+except ValueError:
+    print("Invalid input. Please enter a number corresponding to the station.")        
