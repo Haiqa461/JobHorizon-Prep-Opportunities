8f25ac1fd19660ca5a0dc406595cb385acbf8fca
Ahmed Shafique <99950606+AhmedShafique313@users.noreply.github.com>
2025-01-18 17:34:07 +0500

    simple program is working good

--- supabase based metro ticket project/project.py
+++ supabase based metro ticket project/project.py
@@ -6,8 +6,19 @@ def ticket_counter():
     print(f'Hi, {name_passenger}, Select stations from the list below:')
     for index, (key, value) in enumerate(stations.items(), start=1):
         print(f'{index}. {key} Rs.{value}')
-    
-
-
+    try:
+        choice = int(input('Enter the number of the station you want to select: '))
+        match choice:
+            case 1:
+                station_name = 'UET'
+            case 2:
+                station_name = 'Sultan Pura'
+            case 3:
+                station_name = 'Railwat Station'
+        
+        station_price = stations[station_name]
+        print(f"You selected: {station_name} with a ticket price of Rs. {station_price}.")
+    except ValueError:
+        print("Invalid input. Please enter a number corresponding to the station.")        
 
 ticket_counter()
